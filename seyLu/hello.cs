9256f08f5f88419665d5a0c16dea49cecb5876f5
ErasableSky <judeasharp@hotmail.com>
2023-08-15 19:53:56 +0100

    feat: it works on my machine

--- seyLu/hello.cs
+++ seyLu/hello.cs
@@ -2,7 +2,7 @@ using System;
 
 public class HelloWorld
 {
-    static public void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
     }

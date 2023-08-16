dd1d176b315de253bd9146c72160d7e88ae76a3b
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-17 04:17:10 +0800

    chore: update to 4 spaces instead of 2

--- seyLu/fcc-csharp/Program.cs
+++ seyLu/fcc-csharp/Program.cs
@@ -5,19 +5,19 @@ using System.Threading.Tasks;
 
 namespace Giraffe
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      string phrase = "Giraffe Academy" + " is cool";
-      Console.WriteLine(phrase.Length);
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy" + " is cool";
+            Console.WriteLine(phrase.Length);
 
-      char[] helloChars = { 'h', 'e', 'l', 'l', 'o' };
-      string helloStr = new string(helloChars);
+            char[] helloChars = { 'h', 'e', 'l', 'l', 'o' };
+            string helloStr = new string(helloChars);
 
-      Console.WriteLine(helloStr.Substring(1, 3));
+            Console.WriteLine(helloStr.Substring(1, 3));
 
-      Console.ReadLine();
+            Console.ReadLine();
+        }
     }
-  }
 }
